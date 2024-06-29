f9701c5d5af1394bf3a2312add8168807e43df7a
Luisf66 <Luisf311220@gmail.com>
2024-06-29 20:54:49 -0300

    exercicio 1016 finalizado

---
+++ 1016.cpp
@@ -0,0 +1,13 @@
+#include "iostream"
+
+using namespace std;
+
+int main ()
+{
+    int velocidade = 0;
+
+    cin >> velocidade;
+
+    cout << velocidade * 2 << " minutos" << endl;
+    return 0;
+}
